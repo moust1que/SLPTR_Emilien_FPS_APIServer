75b8cbf807889ce53247f06fe89f960e738c7c10
moust1que <emiliensalpeteur@gmail.com>
2025-12-01 00:07:39 +0100

    Update app.js
    nodemailer setup

--- app.js
+++ app.js
@@ -14,14 +14,11 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    host: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    family: 4
+    }
 });
 
 const pool = createPool({
